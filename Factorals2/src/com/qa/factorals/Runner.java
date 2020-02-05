6db041337851f02950ef489b8c039ec9eed7173b
malik <malikbensalem@outlook.com>
2020-02-05 15:17:33 +0000

    malik=driver

---
+++ Factorals2/src/com/qa/factorals/Runner.java
@@ -0,0 +1,14 @@
+package com.qa.factorals;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println(Factoral.factoral(100));
+		
+		
+	}
+
+}
