4309e3ced2453ce79bf422b0c128000accce1ea1
Wozack <wozniakjack96@gmail.com>
2020-02-05 16:09:42 +0000

    Added DogeCompetition1 driver=jack

---
+++ DogeCompetition1/src/com/qa/doggos/Runner.java
@@ -0,0 +1,16 @@
+package com.qa.doggos;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 15;
+		
+		Doggers.placements(x);
+		
+		
+		
+	}
+
+}
